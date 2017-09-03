a487235e52b34800240ea729f2e70d65d39bd9ac
Marco Lara <marco@marcolara.com>
2017-09-02 17:31:49 -0700

    Added password recovery email

--- tattoosoft-oauth-resource/src/main/java/com/tattoosoft/resource/controller/AccountController.java
+++ tattoosoft-oauth-resource/src/main/java/com/tattoosoft/resource/controller/AccountController.java
@@ -5,16 +5,17 @@ package com.tattoosoft.resource.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tattoosoft.business.form.ForgotPasswordForm;
+import com.tattoosoft.business.service.RegistrationService;
 import com.tattoosoft.business.web.rest.RestResponse;
 
 /**
@@ -26,12 +27,16 @@ import com.tattoosoft.business.web.rest.RestResponse;
 public class AccountController {
 	public static final String REQUEST_MAPPING = "/account";
 	
+	@Autowired
+	RegistrationService registrationService;
+	
 	// Handeling the reset of password
     @RequestMapping(method = RequestMethod.PUT)
     public RestResponse<Object> resetPassword(@Valid @RequestBody ForgotPasswordForm form, BindingResult result) throws NoSuchMethodException, SecurityException, MethodArgumentNotValidException {
     	if (result.hasErrors()){
 			throw new MethodArgumentNotValidException(new MethodParameter(this.getClass().getDeclaredMethod("resetPassword", ForgotPasswordForm.class, BindingResult.class), 0), result);
     	}
+    	registrationService.recoverPasswordByEmail(form.getEmailAddress());
         return RestResponse.forSuccess();
     }
 }
